e6513d5ffc7f4a94aaadfc2e3692452e4d35c7ec
Artinfo <1269788142@qq.com>
2018-01-31 20:43:25 +0800

    将进程状态由S转变为Z，经过指定时长之后自动恢复为S

---
+++ kernel_modules/proc_s2z.c
@@ -0,0 +1,54 @@
+/**
+ * 将进程的状态由normal(S)-->Z状态，经过指定的时间后恢复
+ * 
+ * 编译方法：
+ * Makefile的内容：
+ * obj-m := proc_s2z.o
+ * all:
+ *  $(MAKE) -C /lib/modules/$(shell uname -r)/build M=$(PWD) modules
+ * clean:
+ *  $(MAKE) -C /lib/modules/$(shell uname -r)/build M=$(PWD) clean
+ * 执行make，会生成一个proc_s2z.ko文件
+ * 
+ * 使用方法：
+ * insmod ./proc_s2z.ko pid=xxxx delay=10  (表示Z状态持续10秒后恢复为S状态)
+ * rmmod proc_s2z.ko  (卸载内核模块)
+ */
+
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/sched.h>
+#include <linux/delay.h>
+
+MODULE_AUTHOR("ChenDong");
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("This module make process from S to Z, then back to S");
+
+static int pid = -1;
+static int delay = -1;
+module_param(pid, int, S_IRUGO);
+module_param(delay, int, S_IRUGO);
+
+static int change_process_state(void)
+{
+    struct task_struct *p;
+    for_each_process(p)
+    {
+        if(p->pid == pid)
+        {
+            set_task_state(p, EXIT_ZOMBIE);
+            mdelay(delay * 1000);
+            set_task_state(p, TASK_INTERRUPTIBLE);
+            return 0;
+        }
+    }
+    return 0;
+}
+
+static void nothing(void)
+{
+}
+
+module_init(change_process_state);
+module_exit(nothing);
