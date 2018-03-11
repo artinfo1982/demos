8798105389df91f528428f7c8f59e3139e6463cd
Artinfo <1269788142@qq.com>
2018-03-11 14:57:51 +0800

    Update test.py

--- ai/plr/test.py
+++ ai/plr/test.py
@@ -4,12 +4,11 @@
 车牌识别检测测试程序，可以识别检测车牌号码、车牌颜色
 '''
 
-import pipline as pp
-import colourDetection as cd
+from src import pipline as pp
+from src import typeDistinguish as td
 import cv2
 import sys
 
 inputFile = sys.argv[1]
 image = cv2.imread(inputFile)
 pp.SimpleRecognizePlate(image)
-cd.judge_plate_color(image)
