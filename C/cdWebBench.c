2e9e20edf5d7a93ee041a56935ff5a53b5ec068b
Artinfo <1269788142@qq.com>
2018-09-12 19:33:26 +0800

    Update cdWebBench.c

--- C/cdWebBench.c
+++ C/cdWebBench.c
@@ -31,5 +31,27 @@ gcc cdWebBench.c -o cdWebBench -O3
 #include <signal.h>
 
 /*
-volatile的主要目的是在程序运行期，当需要该变量时
+volatile的主要目的是在程序运行期，当需要该变量时，强制从内存中读取当前最新的值，而不是读缓存的旧值。
+一般用于多进程编程，当一个变量会反复被很多进程读写。
+timerexpired变量在本程序中的作用是标识该进程是否已经达到设定的测试时长，0表示时间没到，1表示时间到了
 */
+volatile static int timerexpired = 0;
+static int success = 0;
+static int fail = 0;
+static int bytes = 0;
+//Allow: GET, POST, PUT, DELETE
+#define METHOD_GET 0
+#define METHOD_POST 1
+#define METHOD_PUT 2
+#define METHOD_DELETE 3
+static int method = METHOD_GET;
+static int clients = 1;
+static int force = 0;
+static int force_reload = 0;
+static int proxyport = 80;
+static char *proxyhost = NULL;
+static int benchtime = 30;
+/*
+定义管道，用于父子进程之间的交互，在后续详细解释。
+*/
+static int mypipe[2];
