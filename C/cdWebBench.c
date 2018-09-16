951e5f7d416196550ce75422ce9f6093194a2187
Artinfo <1269788142@qq.com>
2018-09-16 21:27:34 +0800

    Update cdWebBench.c

--- C/cdWebBench.c
+++ C/cdWebBench.c
@@ -397,3 +397,109 @@ void build_request(const char *url)
 		strcat(request, req_body);
 	}
 }
+
+static int bench(void)
+{
+	int i, j, k;	
+	pid_t pid = 0;
+	FILE *f;
+
+  	/* check avaibility of target server */
+  	i = Socket(proxyhost == NULL ? host : proxyhost, proxyport);
+	if (i < 0)
+	{ 
+		fprintf(stderr, "\nConnect to server failed. Aborting benchmark.\n");
+		return 1;
+	}
+	close(i);
+	/*
+	*/
+  if(pipe(mypipe))
+  {
+	  perror("pipe failed.");
+	  return 3;
+  }
+
+  /* not needed, since we have alarm() in childrens */
+  /* wait 4 next system clock tick */
+  /*
+  cas=time(NULL);
+  while(time(NULL)==cas)
+        sched_yield();
+  */
+
+  /* fork childs */
+  for(i=0;i<clients;i++)
+  {
+	   pid=fork();
+	   if(pid <= (pid_t) 0)
+	   {
+		   /* child process or error*/
+	           sleep(1); /* make childs faster */
+		   break;
+	   }
+  }
+
+  if( pid< (pid_t) 0)
+  {
+          fprintf(stderr,"problems forking worker no. %d\n",i);
+	  perror("fork failed.");
+	  return 3;
+  }
+
+  if(pid== (pid_t) 0)
+  {
+    /* I am a child */
+    if(proxyhost==NULL)
+      benchcore(host,proxyport,request);
+         else
+      benchcore(proxyhost,proxyport,request);
+
+         /* write results to pipe */
+	 f=fdopen(mypipe[1],"w");
+	 if(f==NULL)
+	 {
+		 perror("open pipe for writing failed.");
+		 return 3;
+	 }
+	 /* fprintf(stderr,"Child - %d %d\n",speed,failed); */
+	 fprintf(f,"%d %d %d\n",speed,failed,bytes);
+	 fclose(f);
+	 return 0;
+  } else
+  {
+	  f=fdopen(mypipe[0],"r");
+	  if(f==NULL) 
+	  {
+		  perror("open pipe for reading failed.");
+		  return 3;
+	  }
+	  setvbuf(f,NULL,_IONBF,0);
+	  speed=0;
+          failed=0;
+          bytes=0;
+
+	  while(1)
+	  {
+		  pid=fscanf(f,"%d %d %d",&i,&j,&k);
+		  if(pid<2)
+                  {
+                       fprintf(stderr,"Some of our childrens died.\n");
+                       break;
+                  }
+		  speed+=i;
+		  failed+=j;
+		  bytes+=k;
+		  /* fprintf(stderr,"*Knock* %d %d read=%d\n",speed,failed,pid); */
+		  if(--clients==0) break;
+	  }
+	  fclose(f);
+
+  printf("\nSpeed=%d pages/min, %d bytes/sec.\nRequests: %d susceed, %d failed.\n",
+		  (int)((speed+failed)/(benchtime/60.0f)),
+		  (int)(bytes/(float)benchtime),
+		  speed,
+		  failed);
+  }
+  return i;
+}
