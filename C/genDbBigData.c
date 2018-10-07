27b7740e9d6e31f15dc68723a12e4de85e6b5c54
Artinfo <1269788142@qq.com>
2018-10-07 08:51:02 +0800

    Update genDbBigData.c

--- C/genDbBigData.c
+++ C/genDbBigData.c
@@ -35,4 +35,38 @@ int main(int argc, char *argv[])
     return 1;
   }
   
+  long loop = atol(argv[1]);
+  long u_len = atol(argv[2]);
+  long u_num = atol(argv[3]);
+  long all_unit_len = (u_len + 1) * u_num;
+  char *file = argv[4];
+  long i;
+  int index_len;
+  long row_len;
+  int fd = -1, ret = -1;
+  long offset = 0L;
+  
+  if ((fd = open(file, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR)) < 0)
+  {
+    printf("Can not open file: %s\n", file);
+    return 1;
+  }
+  
+  char *p_unit = (char *)malloc((u_len + 2) * sizeof(char));
+  memset(p_unit, 0x0, u_len + 2);
+  memcpy(p_unit, SEPARATOR, 1);
+  for (i=1; i<u_len+1; i++)
+    memcpy(p_unit + i, STR_UNIT + 1);
+  
+  char *p_all_unit = (char *)malloc((all_unit_len + 1) * sizeof(char));
+  memset(p_all_unit, 0x0, all_unit_len + 1);
+  
+  for (i=0; i<u_num; i++)
+    memcpy(p_all_unit + (u_len + 1) * i, p_unit, u_len + 1);
+  
+  
+  
+  
+  
+  
 }
