0b8bcbe586903cfc6e72fb304e032b8154176ae1
Artinfo <1269788142@qq.com>
2018-11-22 22:33:17 +0800

    Create PlrIntoSrc.cpp

---
+++ opencv/plate_licence_recognition/put_plate_img_into_background/src/PlrIntoSrc.cpp
@@ -0,0 +1,10 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include "PlrIntoSrc.h"
+
+using namespace cv;
+
+const int licence_chr_class[MAX_PLR_FILE_NUM][8] = {
+};
