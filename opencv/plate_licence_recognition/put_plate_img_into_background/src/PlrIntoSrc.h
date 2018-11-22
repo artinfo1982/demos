b77315aadac0b7cdf7560d283c8d39cf14103169
Artinfo <1269788142@qq.com>
2018-11-22 22:30:12 +0800

    Update PlrIntoSrc.h

--- opencv/plate_licence_recognition/put_plate_img_into_background/src/PlrIntoSrc.h
+++ opencv/plate_licence_recognition/put_plate_img_into_background/src/PlrIntoSrc.h
@@ -26,3 +26,30 @@ using namespace cv;
 //xml物体信息循环体缓存大小
 #define LOOP_BUF_SIZE 1024
 
+//拼接xml示例澹?
+//XML_A + 文件名编号 + XML_B + 文件名编号 + XML_C + [ XML_LOOP_A + 类别名 + XML_LOOP_B + xmin
+//+ XML_LOOP_C + ymin + XML_LOOP_D + xmax + XML_LOOP_E + ymax + XML_LOOP_F ] + XML_D
+#define XML_A "<annotation><folder>plr</folder><filename>"
+#define XML_B "</filename><path>/home/cd/plr/VOCdevkit/VOC2007/JPEGImages/"
+#define XML_C ".jpg</path><source><database>Unknown</database></source><size><width>512</width><height>512</height><depth>3</depth></size><segmented>0</segmented>"
+#define XML_LOOP_A "<object><name>"
+#define XML_LOOP_B "</name><pose>Unspecified</pose><truncated>0</truncated><difficult>0</difficult><bndbox><xmin>"
+#define XML_LOOP_C "</xmin><ymin>"
+#define XML_LOOP_D "</ymin><xmax>"
+#define XML_LOOP_E "</xmax><ymax>"
+#define XML_LOOP_F "</ymax></bndbox></object>"
+#define XML_D "</annotation>"
+
+void insert_plateImg_into_srcImg(int x, int y, int width, int height, Mat &bg, Mat &plr);
+int write_label_xml(const char *filename, int index);
+void process_single_background_image(const char *bgFile, const char *plrPath, int block_num, const char*outputFile);
+
+extern char *p;
+extern char *buf;
+extern int plr_index;
+extern char *plr_name;
+extern int class_array[TOTAL_BLOCK_NUM][8];
+extern int x_array[TOTAL_BLOCK_NUM][10];
+extern int y_array[TOTAL_BLOCK_NUM][2];
+
+#endif
