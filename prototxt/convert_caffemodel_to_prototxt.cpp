9203796b2b09116953d3cedb9d512c5f3f7dc304
Artinfo <artinfo1982@126.com>
2018-11-24 22:58:35 +0800

    Update convert_caffemodel_to_prototxt.cpp

--- prototxt/convert_caffemodel_to_prototxt.cpp
+++ prototxt/convert_caffemodel_to_prototxt.cpp
@@ -14,4 +14,24 @@ int main(int argc, char *argv[])
     printf("Usage: %s caffeModelFileName prototxtFileName\n", argv[0]);
     exit(1);
   }
+  char *model = argv[1];
+  char *proto = argv[2];
+  Phase phase = TEST;
+  Caffe::set_mode(Caffe::GPU);
+  Caffe::SetDevice(0);
+  boost::shared_ptr< Net<float> > net(new caffe::Net<float>(proto, phase));
+  NetParameter param;
+  ReadNetParamsFromBinaryFileOrDie(model, &param);
+  int num_layers = param.layer_size();
+  for (int i = 0; i < num_layers; ++i)
+  {
+    cout << "Layer " << i << " : " << param.layer(i).name() << "\t" << param.layer(i).type();
+    if (param.layer(i).type() == "Convolution")
+    {
+      ConvolutionParameter conv_param = param.layer(i).convolution_param();
+      printf("kernel size : %d\n", conv_param,kernel_size());
+    }
+  }
+  
+  return 0;
 }
