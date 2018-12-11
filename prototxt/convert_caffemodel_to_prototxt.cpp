b57786d69d9e832eb69cf1e2d4f1f106491a40d4
Artinfo <artinfo1982@126.com>
2018-12-11 23:16:31 +0800

    Update convert_caffemodel_to_prototxt.cpp

--- prototxt/convert_caffemodel_to_prototxt.cpp
+++ prototxt/convert_caffemodel_to_prototxt.cpp
@@ -1,37 +1,54 @@
-//g++ test.cpp -o test -I /usr/local/cuda/include -lprotobuf -lboost_system -lcaffe
+//g++ convert_caffemodel_to_prototxt.cpp -o test -O3 -I /usr/local/cuda/include -lprotobuf -lcaffe -lboost_system
+
 #include "caffe/caffe.hpp"
 #include <string.h>
 #include <vector>
 #include <stdio.h>
+#include <fstream>
 
 using namespace caffe;
 using namespace std;
 
-int main(int argc, char *argv[])
+string filler_parameter_parser(FillerParameter &fp)
 {
-  if (argc != 3)
+  string s;
+  if (fp.has_type())
+    s.append("      type: \"").append(fp.type()).append("\"\n");
+  if (fp.has_value())
+    s.append("      value: \"").append(to_string(fp.value())).append("\n");
+  if (fp.has_min())
+    s.append("      min: \"").append(to_string(fp.min())).append("\n");
+  if (fp.has_max())
+    s.append("      max: \"").append(to_string(fp.max())).append("\n");
+  if (fp.has_mean())
+    s.append("      mean: \"").append(to_string(fp.mean())).append("\n");
+  if (fp.has_std())
+    s.append("      std: \"").append(to_string(fp.std())).append("\n");
+  if (fp.has_sparse())
+    s.append("      sparse: \"").append(to_string(fp.sparse())).append("\n");
+  return s;
+}
+
+string input_parameter_parser(LayerParameter &lp)
+{
+  string s;
+  InputParameter ip = lp.input_param();
+  if (ip.shape_size() > 0)
   {
-    printf("Usage: %s caffeModelFileName prototxtFileName\n", argv[0]);
-    exit(1);
-  }
-  char *model = argv[1];
-  char *proto = argv[2];
-  Phase phase = TEST;
-  Caffe::set_mode(Caffe::GPU);
-  Caffe::SetDevice(0);
-  boost::shared_ptr< Net<float> > net(new caffe::Net<float>(proto, phase));
-  NetParameter param;
-  ReadNetParamsFromBinaryFileOrDie(model, &param);
-  int num_layers = param.layer_size();
-  for (int i = 0; i < num_layers; ++i)
-  {
-    cout << "Layer " << i << " : " << param.layer(i).name() << "\t" << param.layer(i).type();
-    if (param.layer(i).type() == "Convolution")
+    s.append("  input_param {\n");
+    for (int i=0; i<ip.shape_size(); ++i)
     {
-      ConvolutionParameter conv_param = param.layer(i).convolution_param();
-      printf("kernel size : %d\n", conv_param,kernel_size());
+      BlobShape bs = ip.shape(i);
+      s.append("    shape: {\n");
+      if (bs.dim_size() > 0)
+      {
+        for (int j=0; j<bs.dim_size(); ++j)
+          s.append("      dim: ").append(to_string(bs.dim(j))).append("\n");
+      }
     }
+    s.append("    }\n  }\n");
+    return s;
   }
-  
-  return 0;
+  else
+    return "";
 }
