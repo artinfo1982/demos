be8b0a573a4767f05372f85f91fe4d575d63dddc
Artinfo <artinfo1982@126.com>
2018-12-15 21:46:49 +0800

    Update convert_caffemodel_to_prototxt.cpp

--- prototxt/convert_caffemodel_to_prototxt.cpp
+++ prototxt/convert_caffemodel_to_prototxt.cpp
@@ -62,4 +62,48 @@ string data_parameter_parser(LayerParameter &lp)
     s.append("    source: \"").append(dp.source()).append("\"\n");
   if (dp.has_mean_file())
     s.append("    mean_file: \"").append(dp.mean_file()).append("\"\n");
+  if (dp.has_batch_size())
+    s.append("    batch_size: ").append(to_string(dp.batch_size())).append("\n");
+  if (dp.has_crop_size())
+    s.append("    crop_size: ").append(to_string(dp.crop_size())).append("\n");
+  if (dp.has_mirror())
+    s.append("    mirror: ").append(to_string(dp.mirror())).append("\n");
+  if (dp.has_rand_skip())
+    s.append("    rand_skip: ").append(to_string(dp.rand_skip())).append("\n");
+  if (dp.has_scale())
+    s.append("    scale: ").append(to_string(dp.scale())).append("\n");
+  if (dp.has_prefetch())
+    s.append("    prefetch: ").append(to_string(dp.prefetch())).append("\n");
+  s.append("  }\n");
 }
+
+string convolution_parameter_parser(LayerParameter &lp)
+{
+  string s;
+  ConvolutionParameter cp = lp.convolution_param();
+  s.append("  convolution_param {\n");
+  if (cp.has_num_output())
+    s.append("    num_output: ").append(to_string(cp.num_output())).append("\n");
+  if (cp.kernel_size_size() > 0)
+    s.append("    kernel_size: ").append(to_string(cp.kernel_size_size())).append("\n");
+  if (cp.stride_size() > 0)
+    s.append("    stride_size: ").append(to_string(cp.stride_size())).append("\n");
+  if (cp.pad_size() > 0)
+    s.append("    pad_size: ").append(to_string(cp.pad_size())).append("\n");
+  if (cp.has_weight_filler())
+  {
+    s.append("    weight_filler {\n");
+    FillerParameter fp = cp.weight_filler();
+    s.append(filler_parameter_parser(fp)).append("    }\n")
+  }
+  if (cp.has_bias_filler())
+  {
+    s.append("    bias_filler {\n");
+    FillerParameter fp = cp.bias_filler();
+    s.append(filler_parameter_parser(fp)).append("    }\n")
+  }
+    
+}
+
+
+
