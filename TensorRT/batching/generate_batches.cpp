bd0b43e222193a69e4f11b00b621714b0d60d547
Artinfo <artinfo1982@126.com>
2019-01-02 23:09:32 +0800

    Create generate_batches.cpp

---
+++ TensorRT/batching/generate_batches.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "opencv2/opencv.hpp"
+#include <fstream>
+#include <string>
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+
+#define BATCH 100
+#define B_FILE_NUM 30
+#define PIC_SIZE 300
+#define PIX_NUM 90000
+
+int main(int argc, char *argv[])
+{
+}
