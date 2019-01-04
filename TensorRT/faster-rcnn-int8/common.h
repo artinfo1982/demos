589b68d89d57cf62ada7035322a8f1a289e5e193
Artinfo <artinfo1982@126.com>
2019-01-04 22:09:46 +0800

    Update common.h

--- TensorRT/faster-rcnn-int8/common.h
+++ TensorRT/faster-rcnn-int8/common.h
@@ -1,30 +1,28 @@
 #ifndef _TRT_COMMON_H_
 #define _TRT_COMMON_H_
+
 #include "NvInfer.h"
 #include <string>
 #include <vector>
 #include <fstream>
 #include <cassert>
 #include <iostream>
+
 #define CHECK(status)									\
 {														\
-	if (status != 0)									\
-	{													\
-		std::cout << "Cuda failure: " << status;		\
-		abort();										\
-	}													\
+    if (status != 0)									\
+    {													\
+	std::cout << "Cuda failure: " << status;		\
+	abort();										\
+    }													\
 }
 
-
-// Logger for GIE info/warning/errors
 class Logger : public nvinfer1::ILogger			
 {
-    public:
-	void log(nvinfer1::ILogger::Severity severity, const char* msg) override
-	{
-		// suppress info-level messages
+public:
+    void log(nvinfer1::ILogger::Severity severity, const char* msg) override
+    {
         if (severity == Severity::kINFO) return;
-
         switch (severity)
         {
             case Severity::kINTERNAL_ERROR: std::cerr << "INTERNAL_ERROR: "; break;
@@ -34,9 +32,9 @@ class Logger : public nvinfer1::ILogger
             default: std::cerr << "UNKNOWN: "; break;
         }
         std::cerr << msg << std::endl;
-	}
+    }
 };
 
 std::string locateFile(const std::string& input, const std::vector<std::string> & directories);
 void readPGMFile(const std::string& fileName,  uint8_t *buffer, int inH, int inW);
-#endif // _TRT_COMMON_H_
+#endif
