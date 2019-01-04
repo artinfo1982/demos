afd67f59a5a4d2db89250d7a81ccc0c5ca8a57c5
Artinfo <artinfo1982@126.com>
2019-01-04 21:34:37 +0800

    Update generate_batches.cpp

--- TensorRT/batching/generate_batches.cpp
+++ TensorRT/batching/generate_batches.cpp
@@ -47,6 +47,49 @@ int main(int argc, char *argv[])
   string prefix = "batch";
   for (i = 0; i < B_FILE_NUM; ++i)
   {
-    
+    s_batch_file_name = argv[2] + prefix + to_string(i);
+    batch_file_name = s_batch_file_name.c_str();
+    FILE* file = fopen(batch_file_name, "w+");
+    if (0 == file)
+    {
+      cout << "batch file is not exist, filename: " << batch_file_name << endl;
+      exit(1);
+    }
+    fwrite(shape, sizeof(int), 4, file);
+    begin = i * 100;
+    for (j = begin; j < begin + 100; ++j)
+    {
+      s_img_file_name = argv[1] + to_string(j) + ".jpg";
+      img_file_name = s_img_file_name.c_str();
+      Mat src = imread(img_file_name);
+      Mat img;
+      resize(src, img, dsize, 0, 0, INTER_LINEAR);
+      memset(r, 0x0, PIX_NUM);
+      memset(g, 0x0, PIX_NUM);
+      memset(b, 0x0, PIX_NUM);
+      k = 0;
+      for (m = 0; m < PIC_SIZE; ++m)
+      {
+        for (n = 0; n < PIC_SIZE; ++n)
+        {
+          Vec3b pix = img.at<Vec3b>(m, n);
+          r[k] = pix[0];
+          g[k] = pix[1];
+          b[k] = pix[2];
+          k++;
+        }
+      }
+      fwrite(r, sizeof(float), PIX_NUM, file);
+      fwrite(g, sizeof(float), PIX_NUM, file);
+      fwrite(b, sizeof(float), PIX_NUM, file);
+      src.release();
+      img.release();
+    }
+    fwrite(p, sizeof(float), PIX_NUM, file);
+    p += PIX_NUM;
+    if (0 != file)
+      fclose(file);
+    idx++;
   }
+  return 0;
 }
