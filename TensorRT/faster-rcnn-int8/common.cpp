803c348169c60c997df2eac720284e52e8f8f86b
Artinfo <artinfo1982@126.com>
2019-01-04 22:07:51 +0800

    Update common.cpp

--- TensorRT/faster-rcnn-int8/common.cpp
+++ TensorRT/faster-rcnn-int8/common.cpp
@@ -3,7 +3,7 @@
 std::string locateFile(const std::string& input, const std::vector<std::string> & directories)
 {
     std::string file;
-	const int MAX_DEPTH{10};
+    const int MAX_DEPTH{10};
     bool found{false};
     for (auto &dir : directories)
     {
@@ -18,17 +18,16 @@ std::string locateFile(const std::string& input, const std::vector<std::string> 
         if (found) break;
         file.clear();
     }
-
     assert(!file.empty() && "Could not find a file due to it not existing in the data directory.");
     return file;
 }
 
 void readPGMFile(const std::string& fileName,  uint8_t *buffer, int inH, int inW)
 {
-	std::ifstream infile(fileName, std::ifstream::binary);
+    std::ifstream infile(fileName, std::ifstream::binary);
     assert(infile.is_open() && "Attempting to read from a file that is not open.");
-	std::string magic, h, w, max;
-	infile >> magic >> h >> w >> max;
-	infile.seekg(1, infile.cur);
-	infile.read(reinterpret_cast<char*>(buffer), inH*inW);
+    std::string magic, h, w, max;
+    infile >> magic >> h >> w >> max;
+    infile.seekg(1, infile.cur);
+    infile.read(reinterpret_cast<char*>(buffer), inH*inW);
 }
