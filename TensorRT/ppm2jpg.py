60400909b0b8a83da1e5260f391573610f5a5dda
Artinfo <artinfo1982@126.com>
2019-01-05 16:57:09 +0800

    Update ppm2jpg.py

--- TensorRT/ppm2jpg.py
+++ TensorRT/ppm2jpg.py
@@ -1,2 +1,17 @@
 '''
+将ppm格式的图片，转为jpg
 '''
+
+#coding=utf-8
+
+import sys
+from PIL import Image
+
+if __name__ == '__main__':
+  if len(sys.argv) != 3:
+    print 'Usage: python ppm2jpg.py ppm_file_name jpg_file_name'
+    exit(1)
+  ppm_file_name = sys.argv[1]
+  jpg_file_name = sys.argv[2]
+  img = Image.open(ppm_file_name)
+  img.save(jpg_file_name)
