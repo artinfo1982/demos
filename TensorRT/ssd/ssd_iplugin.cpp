8036daf2c331b7456928b07be9ab0632d76bf578
Artinfo <artinfo1982@126.com>
2019-01-10 23:20:28 +0800

    Update ssd_iplugin.cpp

--- TensorRT/ssd/ssd_iplugin.cpp
+++ TensorRT/ssd/ssd_iplugin.cpp
@@ -1,8 +1,18 @@
 #include "ssd_iplugin.h"
 
 /*
-below layers must be implemented plugin in TensorRT-4.0.1.6
+below layers must implement plugin in TensorRT-4.0.1.6
 enum class PluginType : int
 {
-}
+  kFASTERRCNN = 0,  //FasterRCNN fused plugin (RPN + ROI pooling)
+  kNORMALIZE = 1,  //Normalize plugin
+  kPERMUTE = 2, //Permute plugin
+  kPRIORBOX = 3,  //PriorBox plugin
+  kSSDDETECTIONOUTPUT = 4,  //SSD DetectionOutput plugin
+  kConcat = 5,  //Concat plugin
+  kPRELU = 6, //YOLO PReLU plugin
+  kYOLOREORG = 7, //YOLO Reorg plugin
+  kYOLOREGION = 8,  //YOLO Region plugin
+  kANCHORGENERATOR = 9 //SSD Grid Anchor Generator
+};
 */
