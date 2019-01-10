e2ece1de12c52e3e22bd7d31e1b9822c7d110d68
Artinfo <artinfo1982@126.com>
2019-01-10 22:50:49 +0800

    Create ssd_iplugin.h

---
+++ TensorRT/ssd/ssd_iplugin.h
@@ -0,0 +1,12 @@
+#ifndef __SSD_IPLUGIN__
+#define __SSD_IPLUGIN__
+
+#include <cassert>
+#include <cmath>
+#include <cstring>
+#include <cuda_runtime_api.h>
+#include <unordered_map>
+#include <iterator>
+#include <algorithm>
+#include <vector>
+#include <memory>
