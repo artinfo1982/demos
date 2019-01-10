bb62bb0c9ea0eb2a225add11373aae36c4209dd0
Artinfo <artinfo1982@126.com>
2019-01-10 22:58:20 +0800

    Update ssd_iplugin.h

--- TensorRT/ssd/ssd_iplugin.h
+++ TensorRT/ssd/ssd_iplugin.h
@@ -10,3 +10,30 @@
 #include <algorithm>
 #include <vector>
 #include <memory>
+
+#include "NvCaffeParser.h"
+#include "NvInferPlugin.h"
+
+using namespace nvinfer1;
+using namespace nvcaffeparser1;
+using namespace plugin;
+
+class PluginFactory : public nvinfer1::IPluginFactory, public nvcaffeparser1::IPluginFactory
+{
+public:
+  virtual nvinfer1::IPlugin* createPlugin(const char* layerName, const nvinfer1::Weights* weights, int nbWeights) override;
+  IPlugin* createPlugin(const char* layerName, const void* serialData, size_t serialLength) override;
+  void(*nvPluginDeleter)(INvPlugin*)
+  {
+    [](INvPlugin* ptr)
+    {
+      ptr->destroy();
+    }
+  };
+  bool isPlugin(const char* name) override;
+  void destroyPlugin();
+  
+  std::unique_ptr<INvPlugin, decltype(nvPluginDeleter)> mPluginRPROI{ nullptr, nvPluginDeleter };
+};
+
+#endif
