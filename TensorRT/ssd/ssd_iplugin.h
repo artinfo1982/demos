e5484625745175ba7920a5fb9fca3fd5c574f85c
Artinfo <artinfo1982@126.com>
2019-01-15 21:04:40 +0800

    Update ssd_iplugin.h

--- TensorRT/ssd/ssd_iplugin.h
+++ TensorRT/ssd/ssd_iplugin.h
@@ -35,9 +35,9 @@ class Reshape : public IPlugin
     int getNbOutputs() const override { return 1; }
     dims getOutputDimensions(int index, const Dims *inputs, int nbInputDims) override
     {
-      assert(nbInputDims == 1);
-      assert(index == 0);
-      assert(inputs[index].nbdims == 3);
+      assert(1 == nbInputDims);
+      assert(0 == index);
+      assert(3 == inputs[index].nbDims);
       assert((inputs[0].d[0]) * (inputs[0].d[1]) % OutC == 0);
       return DimsCHW(OutC, inputs[0].d[0] * inputs[0].d[1] / OutC, inputs[0].d[2]);
     }
@@ -59,6 +59,52 @@ class Reshape : public IPlugin
   protected:
     size_t mCopySize;
 };
+
+class Flatten : public IPlugin
+{
+  public:
+    Flatten() {}
+    Flatten(const void *buffer, size_t size)
+    {
+      assert(size == 3 * sizeof(int));
+      const int *d = *reinterpret_cast<const int *>(buffer);
+      _size = d[0] * d[1] * d[2];
+      dimBottom = DimsCHW{d[0], d[1], d[2]};
+    }
+    inline int getNbOutputs() const override { return 1; }
+    dims getOutputDimensions(int index, const Dims *inputs, int nbInputDims) override
+    {
+      assert(1 == nbInputDims);
+      assert(0 == index);
+      assert(3 == inputs[index].nbDims);
+      _size = inputs[0].d[0] * inputs[0].d[1] * inputs[0].d[2];
+      return DimsCHW(_size, 1, 1);
+    }
+    int initialize() override { return 0; }
+    void terminate() override {}
+    size_t getWorkspaceSize(int) const override { return 0; }
+    int enqueue(int batchSize, const void *const *inputs, void **outputs, void *, cudaStream_t stream) override
+    {
+      CHECK(cudaMemcpyAsync(outputs[0], inputs[0], batchSize * _size * sizeof(float), cudaMemcpyDeviceToDevice, stream));
+      return 0;
+    }
+    size_t getSerializationSize() override { return 3 * sizeof(int); }
+    void serialize(void *buffer) override
+    {
+      int *d = reinterpret_cast<int *>(buffer);
+      d[0] = dimBottom.c();
+      d[1] = dimBottom.h();
+      d[2] = dimBottom.w();
+    }
+    void configure(const Dims *inputs, int nbInputs, const Dims *outputs, int nbOutputs, int) override
+    {
+      dimBottom = DimsCHW(inputs[0].d[0], inputs[0].d[1], inputs[0].d[2]);
+    }
+    
+  protected:
+    DimsCHW dimBottom;
+    int _size;
+}
 
 class SoftmaxPlugin : public IPlugin
 {
