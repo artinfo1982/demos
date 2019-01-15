15e7ed3b704a1b3895a0b14e10cc4931f5cb5301
Artinfo <artinfo1982@126.com>
2019-01-15 20:49:55 +0800

    Update ssd_iplugin.h

--- TensorRT/ssd/ssd_iplugin.h
+++ TensorRT/ssd/ssd_iplugin.h
@@ -22,6 +22,44 @@ using namespace plugin;
 
 void cudaSoftmax(int n, int channels, float *x, float *y);
 
+template <int OutC>
+class Reshape : public IPlugin
+{
+  public:
+    Reshape() {}
+    Reshape(const void *buffer, size_t size)
+    {
+      assert(size == sizeof(mCopySize));
+      mCopySize = *reinterpret_cast<const size_t *>(buffer);
+    }
+    int getNbOutputs() const override { return 1; }
+    dims getOutputDimensions(int index, const Dims *inputs, int nbInputDims) override
+    {
+      assert(nbInputDims == 1);
+      assert(index == 0);
+      assert(inputs[index].nbdims == 3);
+      assert((inputs[0].d[0]) * (inputs[0].d[1]) % OutC == 0);
+      return DimsCHW(OutC, inputs[0].d[0] * inputs[0].d[1] / OutC, inputs[0].d[2]);
+    }
+    int initialize() override { return 0; }
+    void terminate() override {}
+    size_t getWorkspaceSize(int) const override { return mCopySize * 1; }
+    int enqueue(int batchSize, const void *const *inputs, void **outputs, void *, cudaStream_t stream) override
+    {
+      CHECK(cudaMemcpyAsync(outputs[0], inputs[0], mCopySize * batchSize, cudaMemcpyDeviceToDevice, stream));
+      return 0;
+    }
+    size_t getSerializationSize() override { return sizeof(mCopySize); }
+    void serialize(void *buffer) override { *reinterpret_cast<size_t *>(buffer) = mCopySize; }
+    void configure(const Dims *inputs, int nbInputs, const Dims *outputs, int nbOutputs, int) override
+    {
+      mCopySize = inputs[0].d[0] * inputs[0].d[1] * inputs[0].d[2] * sizeof(float);
+    }
+  
+  protected:
+    size_t mCopySize;
+};
+
 class SoftmaxPlugin : public IPlugin
 {
 public:
