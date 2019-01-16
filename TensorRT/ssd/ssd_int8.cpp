18c88206e0bb52e7de7b05aba16287ecc1221695
Artinfo <artinfo1982@126.com>
2019-01-16 23:01:24 +0800

    Update ssd_int8.cpp

--- TensorRT/ssd/ssd_int8.cpp
+++ TensorRT/ssd/ssd_int8.cpp
@@ -104,3 +104,80 @@ class Int8EntropyCalibrator : public IInt8EntropyCalibrator
     std::vector<char> mCalibrationCache;
     DataLoader* _dataloader;
 };
+
+void readPPMFile(const std::string& filename, PPM& ppm)
+{
+    std::ifstream infile(filename, std::ios::in | std::ios::binary);
+    infile.seekg(3, infile.beg);
+    infile >> ppm.w >> ppm.h >> ppm.max;
+    infile.seekg(1, infile.cur);
+    infile.read(reinterpret_cast<char*>(ppm.buffer), ppm.w * ppm.h * 3);
+    infile.close();
+}
+
+void caffeToTRTModel(const std::string& deployFile, const std::string& modelFile, const std::vector<std::string>& outputs, 
+		     unsigned int maxBatchSize, nvcaffeparser1::IPluginFactory* pluginFactory, IHostMemory **modelStream, DataType dataType)
+{
+    IBuilder* builder = createInferBuilder(gLogger);
+    INetworkDefinition* network = builder->createNetwork();
+    ICaffeParser* parser = createCaffeParser();
+    parser->setPluginFactory(pluginFactory);
+    std::cout << "Begin to parse model" <<std::endl;
+    const IBlobNameToTensor* blobNameToTensor = parser->parse(deployFile.c_str(), modelFile.c_str(), *network, 
+							      dataType == DataType::kINT8 ? DataType::kFLOAT : dataType);
+    std::cout << "End to parse model" << std::endl;
+    for (auto& s : outputs)
+	network->markOutput(*blobNameToTensor->find(s.c_str()));
+    builder->setMaxBatchSize(maxBatchSize);
+    builder->setMaxWorkspaceSize(1 << 30);
+    builder->setInt8Mode(true);
+    DataLoader* dataLoader = new DataLoader(maxBatchSize, "/home/cd/TensorRT-4.0.1.6/data/ssd/list.txt", 300, 300, 3);
+    Int8EntropyCalibrator* calibrator = new Int8EntropyCalibrator(dataLoader, maxBatchSize, 300, 300, 3);
+    builder->setInt8Calibrator(calibrator);
+    std::cout << "Begin to build engine..." << std::endl;
+    ICudaEngine* engine = builder->buildCudaEngine(*network);
+    assert(engine);
+    std::cout << "End to build engine..." << std::endl;
+    network->destroy();
+    parser->destroy();
+    (*modelStream) = engine->serialize();
+    engine->destroy();
+    builder->destroy();
+    shutdownProtobufLibrary();
+}
+
+float doInference(IExecutionContext& context, float* inputData, float* detectionOut, int* keepCount, int batchSize)
+{
+    const ICudaEngine& engine = context.getEngine();
+    assert(engine.getNbBindings() == 3);
+    void* buffers[3];
+    float ms = 0.0f;
+    int inputIndex = engine.getBindingIndex(INPUT_BLOB_NAME),
+	outputIndex0 = engine.getBindingIndex(OUTPUT_BLOB_NAME0),
+	outputIndex1 = engine.getBindingIndex(OUTPUT_BLOB_NAME1);
+    CHECK(cudaMalloc(&buffers[inputIndex], batchSize * INPUT_C * INPUT_H * INPUT_W * sizeof(float)));   // data
+    CHECK(cudaMalloc(&buffers[outputIndex0], batchSize * KEEP_TOPK * 7 * sizeof(float)));                  // detection_out
+    CHECK(cudaMalloc(&buffers[outputIndex1], batchSize * sizeof(int))); // keepCount
+    cudaStream_t stream;
+    CHECK(cudaStreamCreate(&stream));
+    CHECK(cudaMemcpyAsync(buffers[inputIndex], inputData, batchSize * INPUT_C * INPUT_H * INPUT_W * sizeof(float), 
+	cudaMemcpyHostToDevice, stream));
+    cudaStreamSynchronize(stream);
+    double start = std::clock();
+    int iter = 1;
+    for(int i=0; i<iter; ++i)
+    {
+        context.enqueue(batchSize, buffers, stream, nullptr);
+        cudaStreamSynchronize(stream);
+    }
+    ms = (std::clock()-start) / (double) CLOCKS_PER_SEC /iter * 1000;
+    std::cout<< "infer total time elapse:  "<< ms << " ms" <<std::endl;
+    CHECK(cudaMemcpyAsync(detectionOut, buffers[outputIndex0], batchSize * KEEP_TOPK * 7 * sizeof(float), cudaMemcpyDeviceToHost, stream));
+    CHECK(cudaMemcpyAsync(keepCount, buffers[outputIndex1], batchSize * sizeof(int), cudaMemcpyDeviceToHost, stream));
+    cudaStreamSynchronize(stream);
+    CHECK(cudaFree(buffers[inputIndex]));
+    CHECK(cudaFree(buffers[outputIndex0]));
+    CHECK(cudaFree(buffers[outputIndex1]));
+    cudaStreamDestroy(stream);
+    return ms;
+}
