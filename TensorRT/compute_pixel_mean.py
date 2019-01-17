f18fb1402caa415f3d3e405d3eb1958df897d2e8
Artinfo <artinfo1982@126.com>
2019-01-17 23:55:43 +0800

    Update compute_pixel_mean.py

--- TensorRT/compute_pixel_mean.py
+++ TensorRT/compute_pixel_mean.py
@@ -17,3 +17,17 @@ r_mean = 0
 count = 0
 
 for img_name in img_list:
+  img_path = os.path.join(img_dir, img_name)
+  img = cv2.imread(img_path)
+  # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) # ro rgb
+  sum_b = sum_b + img[:,:,0].mean()
+  sum_g = sum_g + img[:,:,1].mean()
+  sum_r = sum_r + img[:,:,2].mean()
+  count = count + 1
+  
+b_mean = sum_b / count
+g_mean = sum_g / count
+r_mean = sum_r / count
+img_mean = [b_mean, g_mean, r_mean]
+
+print (img_mean)
