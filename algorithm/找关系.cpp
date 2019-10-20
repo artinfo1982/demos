b6fe92ab902d65869a516be77ffbfab15bc115b1
artinfo1982 <artinfo1982@126.com>
2019-10-21 00:39:34 +0800

    update

--- algorithm/找关系.cpp
+++ algorithm/找关系.cpp
@@ -1,73 +1,116 @@
 #include <iostream>
-#include <map>
 
 using namespace std;
 
 int main()
 {
-	int m, n, i, j, k, mul = 1, sum = 0, x, y;
+	int m, n, i, j, k;
+	int sum = 0;
 	cin >> m >> n;
-	int a[m] = {0};
-	map<int, int> res;
-	int flag = (1+m)*m/2;
-	string line;
-	char c;
-	for (i = 0; i < n; ++i)
-	{
-		cin >> line;
-		x = line.at(0)-65;
-		y = line.at(2)-65;
-		if (a[x] == 0)
-			a[x] = 1;
-		else if (a[y] == 0)
-			a[y] = 1;
-		else if (a[x] > a[y])
-		{
-			cout << "Inconsistency found after " << i+1 << " relations." << endl;
-			return 0;
-		}
-		a[y] = a[x] + 1;
-		mul = 1;
-		sum = 0;
-		for (j = 0; j < m; ++j)
-		{
-			sum += a[j];
-			mul *= a[j];
-		}
-		if (mul != 0 && sum == flag)
-		{
-			cout << "Sorted sequence determined after " << i+1 << " relations: ";
-			res.clear();
-			for (k = 0; k < m; ++k)
-				res.insert(map<int, int>::value_type(a[k], k));
-			for (auto w : res)
-			{
-				c = w.second + 65;
-				cout << c;
-			}
-			cout << endl;
-			return 0;
-		}
-	}
-
+	int a[m][m];
+	int col[m]; // 列向量存储入度
+	int row[m]; // 行向量存储出度
+	int idx = 0;
+	string s;
 	for (i = 0; i < m; ++i)
 	{
-		if (a[i] == 0)
+		col[i] = 0;
+		row[i] = 0;
+		for (j = 0; j < m; ++j)
+			a[i][j] = 0;
+	}
+	bool flag1 = false; // 判定是否有矛盾
+	bool flag2 = true; // 判定入度数组中是否还有0
+	for (k = 0; k < n; ++k)
+	{
+		string line;
+		cin >> line;
+		int x = line.at(0) - 65;
+		int y = line.at(2) - 65;
+		a[x][y] = 1;
+		// 计算所有点的入度，并存入数组
+		for (j = 0; j < m; ++j)
 		{
-			cout << "Sorted sequence cannot be determined." << endl;
+			sum = 0;
+			for (i = 0; i < m; ++i)
+				sum += a[i][j];
+			col[j] = sum;
+		}
+		// 计算所有点的出度，并存入数组
+		for (i = 0; i < m; ++i)
+		{
+			sum = 0;
+			for (j = 0; j < m; ++j)
+				sum += a[i][j];
+			row[i] = sum;
+		}
+		// 如果所有入度都不是0，说明有矛盾的情况。
+		// 如果有入度为0，但对应的出度也为0，说明要么输入还没有结束，要么是孤立点，
+		// 需要判断其余列是否有矛盾
+		flag1 = false;
+		for (i = 0; i < m; ++i)
+		{
+			if ((col[i] == 0) && (row[i] == 0))
+				continue;
+			else if (col[i] == 0)
+			{
+				flag1 = true;
+				break;
+			}
+		}
+		if (!flag1)
+		{
+			cout << "Inconsistency found after " << k+1 << " relations." << endl;
+			return 0;
+		}
+		// 如果入度数组中只有唯一的0，说明存在解的可能性
+		sum = 0;
+		for (i = 0; i < m; ++i)	
+		{
+			if (col[i] == 0)
+				sum++;
+		}
+		if (sum == 1)
+		{
+			while (flag2)
+			{
+				for (i = 0; i < m; ++i)
+				{
+					if (col[i] == 0)
+					{
+						char r = i + 65;
+						s.push_back(r);
+						idx++;
+						for (j = 0; j < m; ++j)
+							a[i][j] = 0;
+						a[i][i] = 1;
+						break;
+					}
+				}
+				for (j = 0; j < m; ++j)
+        		{
+            		sum = 0;
+            		for (i = 0; i < m; ++i)
+                		sum += a[i][j];
+            		col[j] = sum;
+        		}
+				sum = 0;
+				for (i = 0; i < m; ++i)
+				{
+					if (col[i] == 0)
+						sum++;
+				}
+				if (sum != 1)
+					flag2 = false;
+			}
+		}
+		if (idx == m)
+		{
+			cout << "Sorted sequence determined after " << k+1 << " relations: " << s << endl;
 			return 0;
 		}
 	}
-	cout << "Sorted sequence determined after " << m << " relations: ";
-	res.clear();
-	for (k = 0; k < m; ++k)
-		res.insert(map<int, int>::value_type(a[k], k));
-	for (auto w : res)
-	{
-		c = w.second + 65;
-		cout << c;
-	}
-	cout << endl;
-
+	if (idx < m)
+		cout << "Sorted sequence cannot be determined." << endl;
 	return 0;
 }
