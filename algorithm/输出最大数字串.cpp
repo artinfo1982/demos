ee82c1c67d2f3686a2df0a05e8695c3d6e9167d1
artinfo1982 <artinfo1982@126.com>
2019-11-28 23:42:14 +0800

    update

--- algorithm/输出最大数字串.cpp
+++ algorithm/输出最大数字串.cpp
@@ -1,69 +1,99 @@
 #include <iostream>
-#include <map>
+#include <vector>
 
 using namespace std;
 
-int main() {
-	string s;
-	cin >> s;
-	map<int, string> m;
-	for (int i = 0; i < s.size(); ++i) {
-		char c1 = s.at(i);
-		if (c1 != '+' && c1 != '-' && c1 != '.' && (c1 < '0' || c1 > '9')) {
-			s[i] = ' ';
-		}
-		else if (c1 == '+' || c1 == '-') {
-			if (i == 0 || i == s.size()-1) {
-				s[i] = ' ';
-			}
-			int j = 1;
-			while (i+j < s.size() && (s.at(i+j) < '0' || s.at(i+j) > '9')) {
-				j++;
-			}
-			for (int k = i; k < i+j-1; ++k) {
-				s[k] = ' ';
-			}
-		}
-		else if (c1 == '.') {
-			if (i == 0 || i == s.size()-1) {
-				s[i] = ' ';
-			}
-			else if (s.at(i-1) < '0' || s.at(i-1) > '9') {
-				s[i] = ' ';
-			}
-			int m = 1;
-			while (i+m < s.size() && (s.at(i+m) < '0' || s.at(i+m) > '9')) {
-				m++;
-			}
-			for (int k = i; k < i+m-1; ++k) {
-				s[k] = ' ';
-			}
-		}
-	}
+#define MAX(x, y) (x)>(y)?(x):(y)
 
-	for (int i = 0; i < s.size(); ++i) {
-		if (s.at(i) == ' ') {
-			continue;
-		}
-		int j = 1;
-		while (i+j < s.size() && s.at(i+j) != ' ') {
-			j++;
-		}
-		string tmp = s.substr(i, j);
-		if (tmp.size() > 1 && tmp.at(0) == '0') {
-			if (tmp.at(1) != '.') {
-				while (tmp.at(0) == '0') {
-					tmp.erase(0, 1);
+string func(string s)
+{
+	if (s.empty()) {
+		return "";
+	}
+	int size = s.size();
+	vector<pair<int, int>> v;
+	int j = 0, k = 0;
+	int max = 0;
+	string res;
+	for (int i = 0; i < size; ++i) {
+		if (s[i] != '+' && s[i] != '-' && (s[i] < '0' || s[i] > '9')) {
+			v.push_back(make_pair(i, 0));
+		} else if (s[i] == '+' || s[i] == '-') {
+			j = i+1;
+			if (s[j] < '0' || s[j] > '9') {
+				v.push_back(make_pair(i, 0));
+				continue;
+			}
+			while (j < size) {
+				if (s[j] != '+' && s[j] != '-' && s[j] != '.' && (s[j] < '0' || s[j] > '9')) {
+					v.push_back(make_pair(i, j-i));
+					break;
+				} else if (s[j] == '+' || s[j] == '-') {
+					v.push_back(make_pair(i, j-i));
+					break;
+				} else if (s[j] == '.' && j == size-1) {
+					v.push_back(make_pair(i, j-i));
+					break;
+				} else if (s[j] == '.' && (s[j+1] < '0' && s[j+1] > '9')) {
+					v.push_back(make_pair(i, j-i));
+					break;
+				} else if (s[j] == '.' && s[j+1] >= '0' && s[j+1] <= '9') {
+					k = j+1;
+					while (k < size && s[k] >= '0' && s[k] <= '9') {
+						k++;
+					}
+					v.push_back(make_pair(i, k-i));
+					break;
+				} else {
+					j++;
+				}
+			}
+		} else if (s[i] == '.') {
+			v.push_back(make_pair(i, 0));
+		} else {
+			j = i+1;
+			while (j < size) {
+				if (s[j] != '+' && s[j] != '-' && s[j] != '.' && (s[j] < '0' || s[j] > '9')) {
+					v.push_back(make_pair(i, j-i));
+					break;
+				} else if (s[j] == '+' || s[j] == '-') {
+					v.push_back(make_pair(i, j-i));
+					break;
+				} else if (s[j] == '.' && j == size-1) {
+					v.push_back(make_pair(i, j-i));
+					break;
+				} else if (s[j] == '.' && (s[j+1] < '0' && s[j+1] > '9')) {
+					v.push_back(make_pair(i, j-i));
+					break;
+				} else if (s[j] == '.' && s[j+1] >= '0' && s[j+1] <= '9') {
+					k = j+1;
+					while (k < size && s[k] >= '0' && s[k] <= '9') {
+						k++;
+					}
+					v.push_back(make_pair(i, k-i));
+					break;
+				} else {
+					j++;
 				}
 			}
 		}
-		m.insert(map<int, string>::value_type(tmp.size(), tmp));
-		i += j;
 	}
 
-	map<int, string>::iterator it;
-	it = m.end();
-	it--;
-	cout << (*it).second << endl;
+	for (int i = 0; i < v.size(); ++i) {
+		max = MAX(max, v[i].second);
+	}
+
+	for (int i = 0; i < v.size(); ++i) {
+		if (v[i].second == max) {
+			res = s.substr(v[i].first, max);
+		}
+	}
+	return res;
+}
+
+int main()
+{
+	string s = "1234567890abc+132456.26.9ed";
+	cout << func(s) << endl;
 	return 0;
 }
